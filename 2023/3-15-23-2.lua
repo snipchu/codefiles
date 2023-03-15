ccc45d79b9602fc60ea304e5d80d3d0a1e591682
fusha153 <pipandw@gmail.com>
2023-03-15 14:50:00 -0500

    ok final code for today

---
+++ 2023/3-15-23-2.lua
@@ -0,0 +1,16 @@
+local function getinput(text)
+	io.write(text)
+	return tonumber(io.read())
+end
+
+local convert = getinput("Convert °F to °C or convert °C to °F? (1/2): ")
+
+if convert == 1 then
+	local degrees = getinput("\nEnter the temperature in Fahrenheit: ")
+	print(degrees .. "°F is equal to " .. (degrees - 32) * 5 / 9 .. "°C.")
+elseif convert == 2 then
+	local degrees = getinput("\nEnter the temperature in Celcius: ")
+	print(degrees .. "°C is equal to " .. (degrees * 9 / 5) + 32 .. "°F.")
+else
+	print("\nNot a valid choice")
+end
