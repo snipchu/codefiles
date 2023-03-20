0df6c63b05c63a807465c391a9a6e078a52eeca1
fusha153 <pipandw@gmail.com>
2023-03-20 08:52:45 -0500

    added lua code

---
+++ 2023/3.20.23.lua
@@ -0,0 +1,14 @@
+-- get input
+io.write("How many numbers do you want?: ")
+local nthnum = tonumber(io.read())
+
+-- first 2 nums of fib sequence
+local nums = { 0, 1 }
+print(nums[1])
+print(nums[2])
+
+-- print 3rd number, update -> second and third nums of sequence
+for i = 1, nthnum, 1 do
+	print(nums[1] + nums[2])
+	nums = { nums[2], nums[1] + nums[2] }
+end
