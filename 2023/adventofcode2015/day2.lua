6e1e13a758081d84cc95cb7a5654f43a1002dfd5
fusha153 <pipandw@gmail.com>
2023-03-23 15:56:01 -0400

    added adventofcode lua solutions for day 2

--- 2023/adventofcode2015/day2.lua
+++ 2023/adventofcode2015/day2.lua
@@ -1,6 +1,20 @@
 local file = io.open("day2.txt", "r")
+local a = { nil, nil, nil }
+local counter = 1
+local wrapping = 0
+local ribbon = 0
 file = file:read("*all")
 
-local function calculate(l, w, h)
-	return (2 * l * w) + (2 * w * h) + (2 * h * l)
+for num in string.gmatch(file, "%x+") do
+	a[counter] = tonumber(num)
+	counter = counter + 1
+	-- every completed line
+	if counter == 4 then
+		counter = 1
+		table.sort(a)
+		wrapping = wrapping + (2 * a[1] * a[2]) + (2 * a[1] * a[3]) + (2 * a[2] * a[3]) + (a[1] * a[2])
+		ribbon = ribbon + (2 * (a[1] + a[2])) + (a[1] * a[2] * a[3])
+	end
 end
+
+print(ribbon)
