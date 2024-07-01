b6d882ed665bcbc2acbe40393fc3f1bb986a132d
snipchu <pipandw@gmail.com>
2024-07-01 12:03:59 -0500

    arcade session 2

--- 2024/7_1_24.py
+++ 2024/7_1_24.py
@@ -1,54 +1,61 @@
-import keyboard, os, time
-board = [
-    ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
-    ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
-    ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
-    ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
-    ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
-    ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
-    ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
-    ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
-    ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
-    ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X']
-]
+import keyboard, os, time, random
+board = [[' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . '],
+        [' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . '],
+        [' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . '],
+        [' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . '],
+        [' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . '],
+        [' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . '],
+        [' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . '],
+        [' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . '],
+        [' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . '],
+        [' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ']]
 # Y, X, length
 snake = [4,4]
-snakelen = 5
+snakelen = 3
+# all coordinates of snake
 snakebody = [[4,4]]
 # Y, X
 direction = [1,0]
 while True:
+    # clear board
+    os.system('cls')
+    os.system('clear')
+    # reset board
+    for i in range(10):
+        board[i] = [' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ', ' . ']
+
+    # user input
+    if keyboard.is_pressed('w') and direction != [1,0]:
+        direction = [-1,0]
+    if keyboard.is_pressed('a') and direction != [0,1]:
+        direction = [0,-1]
+    if keyboard.is_pressed('s') and direction != [-1,0]:
+        direction = [1,0]
+    if keyboard.is_pressed('d') and direction != [0,-1]:
+        direction = [0,1]
+
+    # update snakebody
+    snake[0] += direction[0]
+    snake[1] += direction[1]
+    snakebody.insert(0, [snake[0],snake[1]])
+    while len(snakebody) > snakelen:
+        snakebody.pop()
+
+    # print snake to board
+    print(snake)
+    if (snake[0] > 0 and snake[1] > 0) and (snake[0] < 10 and snake[1] < 10):
+        for i in range(len(snakebody)):
+            board[snakebody[i][0]][snakebody[i][1]] = " o "
+    else:
+        break
+
     # print board
     for line in board:
         for letter in line:
             print(letter, end="")
         print()
-
-    # user input
-    if keyboard.is_pressed('w'):
-        direction = [-1,0]
-    if keyboard.is_pressed('a'):
-        direction = [0,-1]
-    if keyboard.is_pressed('s'):
-        direction = [1,0]
-    if keyboard.is_pressed('d'):
-        direction = [0,1]
-
-    snake[0] += direction[0]
-    snake[1] += direction[1]
-    snakebody.insert(0, snake)
-    while len(snakebody) > snakelen:
-        snakebody.pop()
-
-    for part in snakebody:
-        board[part[0]][part[1]] = "O"
-
-    print(snakebody)
-    # clear board
     time.sleep(.5)
-    os.system('cls')
-    os.system('clear')
 
-    # reset board
-    for line in board:
-        line = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X']
+os.system('cls')
+os.system('clear')
+print("You lost :(")
