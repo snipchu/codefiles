55bbee6e255c7c2d07ccde75e79c8402bde809c0
snipchu <pipandw@gmail.com>
2024-07-11 23:12:51 -0500

    mp3 player is basically fully functional now YAAY

--- 2024/7_11_24/7_11_24.js
+++ 2024/7_11_24/7_11_24.js
@@ -3,21 +3,19 @@ const songlist = [
   ["Harvey","Her's","./Harvey.mp3"],
   ["Spunky (Makoto's Stage)","Hideki Okugawa","./Spunky.mp3"]
 ]
+let songindex = 0;
 function toggleplay() {
   mp3audio = document.getElementById("mp3audio");
   mp3playbutton = document.querySelector(".mp3playbutton");
   if (mp3audio.paused) {
     mp3audio.play();
-    mp3playbutton.classList.replace("nf-fa-pause", "nf-fa-play");
+    mp3playbutton.classList.replace("nf-fa-play", "nf-fa-pause");
   } else {
     mp3audio.pause();
-    mp3playbutton.classList.replace("nf-fa-play", "nf-fa-pause");
+    mp3playbutton.classList.replace("nf-fa-pause", "nf-fa-play");
   }
 }
-function changevol(amount) {
-  mp3audio.volume += amount;
-  console.log(mp3audio.volume);
-}
+function changevol(amount) { mp3audio.volume += amount; }
 function numtotime(num) {
   min = Math.floor(num/60);
   sec = Math.floor(num%60);
@@ -28,11 +26,24 @@ function numtotime(num) {
 }
 function progbarupdate() {
   progbar = document.getElementById("progbar");
-  progbar.value = (mp3audio.currentTime / mp3audio.duration) * 100;
+  progbar.value = mp3audio.currentTime/mp3audio.duration * 100;
   progbar.previousElementSibling.innerHTML = numtotime(mp3audio.currentTime);
 }
 function changesong(index) {
-  mp3audio.pause();
-  progbarupdate();
-  progbar.nextElementSibling.innerHTML = numtotime(mp3audio.duration);
+  // play next song
+  mp3src = document.getElementById("mp3src");
+  songindex += index;
+  if (songindex == songlist.length) {songindex--;}
+  if (songindex == -1) {songindex++;}
+  mp3src.src = songlist[songindex][2];
+  mp3audio.load();
+  if (index != 0) { mp3audio.play(); }
+
+  // update html after audio loads
+  mp3audio.onloadedmetadata = function() {
+    mp3audio.previousElementSibling.previousElementSibling.innerHTML = songlist[songindex][0];
+    mp3audio.previousElementSibling.innerHTML = songlist[songindex][1];
+    progbar.nextElementSibling.innerHTML = numtotime(mp3audio.duration);
+    progbarupdate();
+  };
 }
