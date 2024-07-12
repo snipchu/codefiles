bf1ae9e3b7a4eb34c766d5dd2408d9520f3b9382
snipchu <pipandw@gmail.com>
2024-07-12 12:42:42 -0500

    shuffle and loop button added to the mp3 player!

--- 2024/7_11_24/7_11_24.js
+++ 2024/7_11_24/7_11_24.js
@@ -7,14 +7,24 @@ let songindex = 0;
 function toggleplay() {
   mp3audio = document.getElementById("mp3audio");
   mp3playbutton = document.querySelector(".mp3playbutton");
-  if (mp3audio.paused) {
-    mp3audio.play();
+  if (mp3audio.paused) { 
     mp3playbutton.classList.replace("nf-fa-play", "nf-fa-pause");
   } else {
-    mp3audio.pause();
     mp3playbutton.classList.replace("nf-fa-pause", "nf-fa-play");
   }
+  mp3audio.paused ? mp3audio.play() : mp3audio.pause();
 }
+function toggleloop() {
+  mp3loopbutton = document.querySelector(".mp3loopbutton");
+  mp3audio.loop = !(mp3audio.loop);
+  if (mp3audio.loop) {
+    mp3loopbutton.classList.replace("nf-md-repeat","nf-md-repeat_once");
+  } else {
+    mp3loopbutton.classList.replace("nf-md-repeat_once","nf-md-repeat");
+  }
+}
+shuffled = false;
+function toggleshuffle() { shuffled = !shuffled; }
 function changevol(amount) { mp3audio.volume += amount; }
 function numtotime(num) {
   min = Math.floor(num/60);
@@ -32,17 +42,18 @@ function progbarupdate() {
 function changesong(index) {
   // play next song
   mp3src = document.getElementById("mp3src");
-  songindex += index;
-  if (songindex == songlist.length) {songindex--;}
-  if (songindex == -1) {songindex++;}
+  !shuffled ? songindex += index : songindex = Math.floor(Math.random()*songlist.length);
+  if (songindex == songlist.length) {songindex = 0;}
+  if (songindex == -1) {songindex = songlist.length-1;}
   mp3src.src = songlist[songindex][2];
   mp3audio.load();
-  if (index != 0) { mp3audio.play(); }
 
   // update html after audio loads
   mp3audio.onloadedmetadata = function() {
-    mp3audio.previousElementSibling.previousElementSibling.innerHTML = songlist[songindex][0];
-    mp3audio.previousElementSibling.innerHTML = songlist[songindex][1];
+    if (index != 0) { mp3audio.play(); }
+    mp3audio.previousElementSibling.innerHTML = songlist[songindex][0];
+    mp3audio.previousElementSibling.previousElementSibling.innerHTML = songlist[songindex][1];
+    mp3audio.parentElement.lastElementChild.innerHTML = (songindex+1)+"/"+(songlist.length);
     progbar.nextElementSibling.innerHTML = numtotime(mp3audio.duration);
     progbarupdate();
   };
